20f9510cbb8da0e279fe3c30ff5a1517b08f7571
KivenC <18721218083@126.com>
2021-04-28 21:33:33 +0800

    make license_control a separate repository

--- python/license_control/license_control/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Author       : ChenKailun
-Date         : 2021-04-25 20:36
-LastEditTime : 2021-04-25 20:36
-FilePath     : /license_control/core/__init__.py
-Description  :
-"""
--- python/license_control/license_control/parser/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Author       : ChenKailun
-Date         : 2021-04-25 20:35
-LastEditTime : 2021-04-25 20:35
-FilePath     : /license_control/parser/__init__.py
-Description  :
-"""
